ea2f807d8a8a0448c83ce4d057970cf5578f0294
camodesigns <camilo970403@gmail.com>
2022-08-31 16:37:59 -0500

    Month2-Section2-DinamycMemory-Exercise4
    Declaration of pointers
    asign value
    delete Dinamyc Memory ptr
    xplain the if test

--- Month2/Pointers_Exercise/Section2-DinamycMemory/Exercise3/Source3.cpp
+++ Month2/Pointers_Exercise/Section2-DinamycMemory/Exercise3/Source3.cpp
@@ -17,6 +17,9 @@ int main()
 	MemoryLeak2(Ptr);
 
 	std::cout << "The first funtion create a memory leak beacuse  it allocates a variable in the heap, but  we cannot acces it outside  the function" << std::endl;
-	std::cout << "the integer will be there indefinitely " << std::endl;
+	std::cout << "the integer will be there for the rest of the execution of the program " << std::endl;
+	std::cout << std::endl;
 
+	std::cout << "The second function create a memory leak, beacuse the dinamyc memory pointer turn in a pointer in the stack, but the memory was not freed, so is  innaccesible and will will be for the rest of the execution of the program " << std::endl;
+	
 }
---
+++ Month2/Pointers_Exercise/Section2-DinamycMemory/Exercise4/Source4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() 
+{
+	int* Ptr;
+	int* HeapPointer = new int;
+	Ptr = HeapPointer;
+	delete HeapPointer;
+
+	if (Ptr)
+	{
+		std::cout << "The pointer is valid" << std::endl;
+	}
+	else 
+	{
+		std::cout << "The pointer is not valid" << std::endl;
+	}
+	std::cout << std::endl;
+
+	std::cout << "This happening beacuse deleting a pointer to dinamyc memory does not turn this to zero" << std::endl;
+	std::cout << "this it's still 'valid' since it's pointing to something, to be specific to the memory space it had before it was erased. " << std::endl;
+}
