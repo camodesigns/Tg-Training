05ffb546deaf67b85c7c75985728e3e0798d50c0
camodesigns <camilo970403@gmail.com>
2022-08-31 17:49:15 -0500

    Month2-Section2-DinamycMemory-Exercise5-9
    Class created
    Constructors created
    Destructor created
    Set Agen an Name created
    Pirnt Students Data crated
    ValidIntInput created
    Test1 Created
    Test two too

---
+++ Month2/Pointers_Exercise/Section2-DinamycMemory/Exercise5-9/Source5-9.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+#include <string>
+class FStudent
+{
+
+private:
+	int Age;
+	char* Name;
+public:
+
+	FStudent()
+	{
+		Name = nullptr;
+		Age = 0;
+	}
+
+
+	FStudent(int EntryAge)
+	{
+		Name = nullptr;
+		Age = EntryAge;
+	}
+
+	~FStudent()
+	{
+		delete Name;
+	}
+	
+	void SetAge(const int NewAge)
+	{
+		Age = NewAge;
+	}
+	
+	void SetName(const char* NewName)
+	{
+		int CharNameLength = 0;
+		if (Name )
+		{
+			delete Name;
+			Name = nullptr;
+		}
+
+		for (int CurrentCharIndex = 0; NewName[CurrentCharIndex] != '\0'; CurrentCharIndex++)
+		{
+			CharNameLength++;
+		}
+
+		Name = new char[CharNameLength + 1];
+		memcpy_s(Name, sizeof(char) * (CharNameLength + 1), NewName, sizeof(char) * (CharNameLength + 1));
+	}
+
+	void PrintStudentData()
+	{
+		std::cout << "Name: " << Name << std::endl;
+		std::cout << "Age: " << Age << std::endl;
+	}
+};
+int ValidIntInput()
+{
+	int Input;
+	std::cin >> Input;
+	while (!std::cin.good() || Input <= 0 )
+	{
+		std::cin.clear();
+		std::cin.ignore(100, '\n');
+		std::cout << "Invalid input, please try again:" << std::endl;
+		std::cin >> Input;
+	}
+	return Input;
+}
+
+
+
+int main()
+{
+
+	FStudent* Student = new FStudent(5);
+	Student->SetName("Santiago");
+	delete Student;
+
+	std::cout << "The student was deleted from memory, but the name remains. The class FStudent tries to call the destructor for delete all the data but the Nname is primitive and cannot be deleted automatically" << std::endl;
+
+
+	std::cout << "How many students do you want? " << std::endl;
+
+	int StudentsNumber = ValidIntInput();
+
+	FStudent* Students = new FStudent[StudentsNumber];
+
+	std::cout << "You  have " << StudentsNumber << " students!" << std::endl;
+
+	for (int CurrentIndex = 0; CurrentIndex < StudentsNumber; CurrentIndex++)
+	{
+		std::cout << "Enter the name of the student number" << CurrentIndex + 1 << ": ";
+		char Name[32] ;
+		std::cin >> Name;
+		
+		if (!std::cin.good() || Name == " " || Name == "")
+		{
+			std::cin.clear();
+			std::cin.ignore(100, '\n');
+			std::cout << "Invalid Input,try again:" << std::endl;
+			std::cin >> Name;
+		}
+
+		std::cout << "Enter the age of the student number" << CurrentIndex + 1 << ": ";
+		int Age = ValidIntInput();
+	
+		Students[CurrentIndex].SetAge(Age);
+		Students[CurrentIndex].SetName(Name);
+	}
+
+
+	std::cout << "Your students:" << std::endl;
+
+	for (int StudentsIndex = 0; StudentsIndex < StudentsNumber; StudentsIndex++)
+	{
+		std::cout << "Student " << StudentsIndex + 1 << std::endl;
+		Students[StudentsIndex].PrintStudentData();
+	}
+	delete[] Students;
+}
