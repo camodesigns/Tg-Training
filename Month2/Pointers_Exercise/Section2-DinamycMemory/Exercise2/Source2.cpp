79ba5bf23d62531a77957ebf3273fcb3c03f9f49
camodesigns <camilo970403@gmail.com>
2022-09-01 14:59:47 -0500

    Update declaration
    update declarations in one line to  more lines

--- Month2/Pointers_Exercise/Section2-DinamycMemory/Exercise2/Source2.cpp
+++ Month2/Pointers_Exercise/Section2-DinamycMemory/Exercise2/Source2.cpp
@@ -2,7 +2,9 @@
 
 int main() 
 {
-	int StackInt1 = 10, StackInt2 = 5, StackInt3 = 12 ;
+	int StackInt1 = 10;
+	int StackInt2 = 5;
+	int	StackInt3 = 12;
 	std::cout << "I created three stack variables:" << std::endl;
 	std::cout << "Their values are:" << std::endl;
 	std::cout << StackInt1 << std::endl;
@@ -14,26 +16,26 @@ int main()
 	std::cout << &StackInt2 << std::endl;
 	std::cout << &StackInt3 << std::endl;
 
-	int* HealpInt1 = new int;
-	*HealpInt1 = 1;
-	int* HealpInt2 = new int;
-	*HealpInt2 = 2;
-	int* HealpInt3 = new int;
-	*HealpInt3 = 3;
+	int* HeapInt1 = new int;
+	*HeapInt1 = 1;
+	int* HeapInt2 = new int;
+	*HeapInt2 = 2;
+	int* HeapInt3 = new int;
+	*HeapInt3 = 3;
 
 	std::cout << "I created three heap variables." << std::endl;
 	std::cout << "Their values are:" << std::endl;
-	std::cout << *HealpInt1 << std::endl;
-	std::cout << *HealpInt2 << std::endl;
-	std::cout << *HealpInt3 << std::endl;
+	std::cout << *HeapInt1 << std::endl;
+	std::cout << *HeapInt2 << std::endl;
+	std::cout << *HeapInt3 << std::endl;
 
 	std::cout << "Their memory addresses are:" << std::endl;
-	std::cout << HealpInt1 << std::endl;
-	std::cout << HealpInt2 << std::endl;
-	std::cout << HealpInt3 << std::endl;
+	std::cout << HeapInt1 << std::endl;
+	std::cout << HeapInt2 << std::endl;
+	std::cout << HeapInt3 << std::endl;
 	
 	std::cout << "I need to manually create and remove the variables from the heap beacuse the heap memory is Dinamic and this variables dont destroy itself.In the other situation with Stack Variables, they destroy themselves when the program terminates." << std::endl;
 	
-	delete HealpInt1, HealpInt2, HealpInt3;
+	delete HeapInt1, HeapInt2, HeapInt3;
 
 }
