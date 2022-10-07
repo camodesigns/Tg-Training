0b51f219573696a4c9a87b904673ff0b772ea5d2
camodesigns <camilo970403@gmail.com>
2022-10-07 12:06:24 -0500

    Full Game updates
    Levels ceration
    size of screen
    Win Game created
    shiip, bullesta and asteroids update

--- Month3/PixelGameEngine/AsteroidGame/AsteroidMain.cpp
+++ Month3/PixelGameEngine/AsteroidGame/AsteroidMain.cpp
@@ -20,7 +20,7 @@ private:
 		float DisplacementY;
 		int Size;
 		float Angle;
-
+		bool BeDead;
 	};
 	std::vector <SpaceObject> VecAsteroid;
 	std::vector<SpaceObject> Bullets;
@@ -30,6 +30,9 @@ private:
 
 	std::vector<std::pair<float, float>> ModelShip;
 	std::vector<std::pair<float, float>> ModelAsteroids;
+	int Level = 1;
+	int Score = 0;
+
 protected:
 
 	void WrapCoordinates(float CoordinatesX, float CoordinatesY, float& WrapedX, float& WrapedY)
@@ -49,7 +52,7 @@ protected:
 		{
 			WrapedY = CoordinatesY + (float)ScreenHeight();
 		}
-		if (CoordinatesY > ScreenWidth())
+		if (CoordinatesY > ScreenHeight())
 		{
 			WrapedY = CoordinatesY - (float)ScreenHeight();
 		}
@@ -59,6 +62,7 @@ protected:
 	{
 		FillRect(0, 0, ScreenWidth(), ScreenHeight(), olc::BLACK);
 	}
+
 	virtual void InitialCharacteristicsPlayer()
 	{
 		//Inicialise player position
@@ -67,7 +71,7 @@ protected:
 		Player.DisplacementX = 0.0f;
 		Player.DisplacementY = 0.0f;
 		Player.Angle = 0.0f;
-
+		Player.BeDead = false;
 		ModelShip =
 		{
 			{0.0f,-15.0f},
@@ -81,14 +85,14 @@ protected:
 		for (int i = 0; i < Verts; i++)
 		{
 			float RandomValue = ((float)rand() / (float)RAND_MAX) * 0.3f;
-			float AsteroidRadius = 1.0f+RandomValue;
+			float AsteroidRadius = 1.0f + RandomValue;
 			float Angle = ((float)i / float(Verts) * 6.28318f);
 			ModelAsteroids.push_back(std::make_pair(AsteroidRadius * sinf(Angle), AsteroidRadius * cosf(Angle)));
 		}
 	}
 
 
-	virtual bool Draw(int PosX, int PosY, olc::Pixel P = olc::WHITE)override
+	virtual bool Draw(int PosX, int PosY,olc::Pixel P = olc::RED)override
 	{
 		float TempX, TempY;
 		WrapCoordinates((float)PosX, (float)PosY, TempX, TempY);
@@ -105,6 +109,7 @@ protected:
 			Player.DisplacementY += -cos(Player.Angle) * 50.0f * ElapsedTime;
 		}
 	}
+
 	void Velocity(float ElapsedTime)
 	{
 		Player.PosX += Player.DisplacementX * ElapsedTime;
@@ -171,26 +176,92 @@ protected:
 		}
 	}
 
-	/*void DrawAsteroids(float ElapsedTime)
+	void HudDisplay()
 	{
-		for (auto& Asteroid : VecAsteroid)
-		{
-			Asteroid.PosX += Asteroid.DisplacementX * ElapsedTime;
-			Asteroid.PosY += Asteroid.DisplacementY * ElapsedTime;
-			WrapCoordinates(Asteroid.PosX, Asteroid.PosY, Asteroid.PosX, Asteroid.PosY);
-			for (int CurrentX = 0; CurrentX < Asteroid.Size; CurrentX++)
-			{
-				for (int CurrentY = 0; CurrentY < Asteroid.Size; CurrentY++)
-				{
-					Draw(Asteroid.PosX + CurrentX, Asteroid.PosY + CurrentY, olc::BLUE);
-				}
-			}
-		}
-	}*/
+		DrawString(8, 8, "Score: " + std::to_string(Score) +"\t" + "Level: " + std::to_string(Level), olc::YELLOW);
+	}
 
-	void AppendNewAsteroids() 
+	bool WinGame()
 	{
-		for (const auto &NewAsteroid : NewAsteroids) 
+		int MaxLevel = 4;
+		if (Level >= MaxLevel)
+		{
+			ModelShip.clear();
+			ModelAsteroids.clear();
+			Bullets.clear();
+
+			DrawString(60, 100, "You Win", olc::GREEN,10);
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+		
+	}
+	void LevelUp()
+	{
+		if (VecAsteroid.empty())
+		{
+			Level += 1;
+			// Level Clear
+			Score += 1000; // Large score for level progression
+			VecAsteroid.clear();
+			Bullets.clear();
+			if (Level == 2) 
+			{
+				VecAsteroid.push_back({ 30.0f * sinf(Player.Angle - 3.14159f / 2.0f) + Player.PosX,
+												  30.0f * cosf(Player.Angle - 3.14159f / 2.0f) + Player.PosY,
+												  10.0f * sinf(Player.Angle), 10.0f * cosf(Player.Angle),(int)15, 0.0f,false });
+
+
+				VecAsteroid.push_back({ 30.0f * sinf(Player.Angle - 3.14159f / 2.0f) + Player.PosX,
+												  30.0f * cosf(Player.Angle - 3.14159f / 2.0f) + Player.PosY,
+												  10.0f * sinf(Player.Angle), 10.0f * cosf(Player.Angle),(int)15, 0.0f,false });
+			}
+			else if (Level ==3) 
+			{
+				VecAsteroid.push_back({ 30.0f * sinf(Player.Angle - 3.14159f / 2.0f) + Player.PosX,
+												  30.0f * cosf(Player.Angle - 3.14159f / 2.0f) + Player.PosY,
+												  10.0f * sinf(Player.Angle), 10.0f * cosf(Player.Angle),(int)15, 0.0f,false });
+
+
+				VecAsteroid.push_back({ 30.0f * sinf(Player.Angle - 3.14159f / 2.0f) + Player.PosX,
+												  30.0f * cosf(Player.Angle - 3.14159f / 2.0f) + Player.PosY,
+												  10.0f * sinf(Player.Angle), 10.0f * cosf(Player.Angle),(int)15, 0.0f,false });
+				VecAsteroid.push_back({ 100.0f * sinf(Player.Angle - 3.14159f / 2.0f) + Player.PosX,
+												  100.0f * cosf(Player.Angle - 3.14159f / 2.0f) + Player.PosY,
+												  10.0f * sinf(Player.Angle), 10.0f * cosf(Player.Angle),(int)15, 0.0f,false });
+			
+			}
+			else if (Level == 4)
+			{
+				VecAsteroid.push_back({ 30.0f * sinf(Player.Angle - 3.14159f / 2.0f) + Player.PosX,
+												  30.0f * cosf(Player.Angle - 3.14159f / 2.0f) + Player.PosY,
+												  10.0f * sinf(Player.Angle), 10.0f * cosf(Player.Angle),(int)15, 0.0f,false });
+
+
+				VecAsteroid.push_back({ 30.0f * sinf(Player.Angle - 3.14159f / 2.0f) + Player.PosX,
+												  30.0f * cosf(Player.Angle - 3.14159f / 2.0f) + Player.PosY,
+												  10.0f * sinf(Player.Angle), 10.0f * cosf(Player.Angle),(int)15, 0.0f,false });
+
+				VecAsteroid.push_back({ 100.0f * sinf(Player.Angle - 3.14159f / 2.0f) + Player.PosX,
+												  100.0f * cosf(Player.Angle - 3.14159f / 2.0f) + Player.PosY,
+												  10.0f * sinf(Player.Angle), 10.0f * cosf(Player.Angle),(int)15, 0.0f,false });
+
+				VecAsteroid.push_back({ 70.0f * sinf(Player.Angle - 3.14159f / 2.0f) + Player.PosX,
+												  70.0f * cosf(Player.Angle - 3.14159f / 2.0f) + Player.PosY,
+												  10.0f * sinf(Player.Angle), 10.0f * cosf(Player.Angle),(int)15, 0.0f,false });
+
+
+			}
+			
+		}
+	}
+
+	void AppendNewAsteroids()
+	{
+		for (const auto& NewAsteroid : NewAsteroids)
 		{
 			VecAsteroid.push_back(NewAsteroid);
 		}
@@ -204,7 +275,7 @@ protected:
 			AsteroidObject.PosX += AsteroidObject.DisplacementX * ElapsedTime;
 			AsteroidObject.PosY += AsteroidObject.DisplacementY * ElapsedTime;
 			WrapCoordinates(AsteroidObject.PosX, AsteroidObject.PosY, AsteroidObject.PosX, AsteroidObject.PosY);
-			DrawWireFrameModel(ModelAsteroids, AsteroidObject.PosX, AsteroidObject.PosY, AsteroidObject.Angle,AsteroidObject.Size);
+			DrawWireFrameModel(ModelAsteroids, AsteroidObject.PosX, AsteroidObject.PosY, AsteroidObject.Angle, AsteroidObject.Size);
 
 		}
 	}
@@ -230,11 +301,11 @@ protected:
 
 			for (auto& Asteroid : VecAsteroid)
 			{
-				if (Collition(Asteroid.PosX, Asteroid.PosY, Asteroid.Size, Bullet.PosX, Bullet.PosY)) 
+				if (Collition(Asteroid.PosX, Asteroid.PosY, Asteroid.Size, Bullet.PosX, Bullet.PosY))
 				{
 					//Asteroid hit
 					Bullet.PosX = -200;
-					if (Asteroid.Size > 4) 
+					if (Asteroid.Size > 4)
 					{
 						float Angle1 = ((float)rand() / (float)RAND_MAX) * 6.283185f;
 						float Angle2 = ((float)rand() / (float)RAND_MAX) * 6.283185f;
@@ -242,12 +313,13 @@ protected:
 						NewAsteroids.push_back({ Asteroid.PosX, Asteroid.PosY,10.0f * sinf(Angle2),10.0f * cosf(Angle2),(int)Asteroid.Size >> 1, 0.0f });
 					}
 					Asteroid.PosX = -100;
+					Score += 100;
 				}
 			}
 		}
 	}
-	
-	
+
+
 
 	void DestroyBullets()
 	{
@@ -258,7 +330,7 @@ protected:
 					{
 						return (BulletDestroy.PosX < 1 || BulletDestroy.PosY < 1 || BulletDestroy.PosX >= ScreenWidth() || BulletDestroy.PosY >= ScreenHeight());
 					});
-			if (Destroy != Bullets.end()) 
+			if (Destroy != Bullets.end())
 			{
 				Bullets.erase(Destroy);
 			}
@@ -281,14 +353,50 @@ protected:
 		}
 	}
 
-	bool Collition(float AsteroidCenterX, float AsteroidCenterY, float Radius, float PosX, float PosY) 
+	bool Collition(float AsteroidCenterX, float AsteroidCenterY, float Radius, float PosX, float PosY)
 	{
-		return sqrt(((PosX - AsteroidCenterX) * (PosX - AsteroidCenterX)) + ((PosY - AsteroidCenterY)* (PosY - AsteroidCenterY))) < Radius;
+		return sqrt(((PosX - AsteroidCenterX) * (PosX - AsteroidCenterX)) + ((PosY - AsteroidCenterY) * (PosY - AsteroidCenterY))) < Radius;
 	}
+
+	void ShipCollition()
+	{
+		for (auto& Asteroid : VecAsteroid)
+		{
+			if (Collition(Asteroid.PosX, Asteroid.PosY, Asteroid.Size, Player.PosX, Player.PosY))
+			{
+				Player.BeDead = true;
+			}
+		}
+	}
+
+	void BeDead()
+	{
+		if (Player.BeDead == true)
+		{
+			VecAsteroid.clear();
+			Bullets.clear();
+			Level = 1;
+
+			Player.PosX = ScreenWidth() / 2.0f;
+			Player.PosY = ScreenHeight() / 2.0f;
+			Player.DisplacementX = 0.0f;
+			Player.DisplacementY = 0.0f;
+			Player.Angle = 0.0f;
+			Player.BeDead = false;
+			ModelShip =
+			{
+				{0.0f,-15.0f},
+				{-5.0f , +5.0f},
+				{+5.0f,+5.0f}
+			};
+
+		}
+	}
+
 
 	virtual bool OnUserCreate()
 	{
-		VecAsteroid.push_back({ 50.0f,50.0f,10.0f,-15.0f,(int)40,0.0f });
+		VecAsteroid.push_back({ 50.0f,50.0f,10.0f,-15.0f,(int)20,0.0f,false });
 		InitialCharacteristicsPlayer();
 		InitialCharateriticsAsteroid();
 		return true;
@@ -298,21 +406,37 @@ protected:
 	{
 
 		BackgroundColor();
-		UpdateAndDrawAsteroids(ElapsedTime);
-		//DrawAsteroids(ElapsedTime);
+		if (WinGame()) 
+		{
+			if (GetKey(olc::Key::SPACE).bReleased)
+			{
+				return false;
+			}				
+			
+		}
+		else 
+		{
+			
+			UpdateAndDrawAsteroids(ElapsedTime);
+			//DrawAsteroids(ElapsedTime);
 
-		TurnLeft(ElapsedTime);
-		TurnRight(ElapsedTime);
-		Aceleration(ElapsedTime);
-		Velocity(ElapsedTime);
-		DrawShip();
-		ShotBullet(ElapsedTime);
-		UpdateAndDrawBullets(ElapsedTime);
-		DestroyBullets();
-		DestroyAsteroids();
-
-		
-		AppendNewAsteroids();
+			TurnLeft(ElapsedTime);
+			TurnRight(ElapsedTime);
+			Aceleration(ElapsedTime);
+			Velocity(ElapsedTime);
+			DrawShip();
+			ShotBullet(ElapsedTime);
+			UpdateAndDrawBullets(ElapsedTime);
+			DestroyBullets();
+			DestroyAsteroids();
+			HudDisplay();
+			ShipCollition();
+			BeDead();
+			AppendNewAsteroids();
+			LevelUp();
+			
+			
+		}
 		return true;
 	}
 
@@ -323,7 +447,7 @@ protected:
 int main()
 {
 	AsteroidGame Game;
-	Game.Construct(720, 350, 2, 2);
+	Game.Construct(320, 200, 2, 2);
 	Game.Start();
 
 	return 0;
